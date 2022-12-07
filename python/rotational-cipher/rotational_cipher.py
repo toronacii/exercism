e5e8aefba40888b10cc8ed5e2b86dcaebc6b28de
José Toro <toronacii@gmail.com>
2022-12-07 15:43:54 -0300

    resolve rotational_cipher

---
+++ python/rotational-cipher/rotational_cipher.py
@@ -0,0 +1,16 @@
+ALPHABETH = 'abcdefghijklmnopqrstuvwxyz'
+
+
+def rotate(text: str, key: int):
+    cipher_alpha = ALPHABETH[key:] + ALPHABETH[:key]
+
+    cipher_text = ''
+    for char in text:
+        try:
+            index = ALPHABETH.index(char.lower())
+            newChar = cipher_alpha[index]
+            cipher_text += newChar if char == char.lower() else newChar.upper()
+        except:
+            cipher_text += char
+
+    return cipher_text
