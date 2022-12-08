80ffc07e8ab475bba06f7245222fb9cfd3802146
José Toro <toronacii@gmail.com>
2022-12-07 23:17:29 -0300

    remove exception for rotational_cipher

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -6,11 +6,11 @@ def rotate(text: str, key: int):
 
     cipher_text = ''
     for char in text:
-        try:
+        if char.lower() in ALPHABETH:
             index = ALPHABETH.index(char.lower())
             newChar = cipher_alpha[index]
             cipher_text += newChar if char == char.lower() else newChar.upper()
-        except:
+        else:
             cipher_text += char
 
     return cipher_text
